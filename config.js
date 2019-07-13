ae55a36bb39e492076bc1949d50b800bf0e2cbb9
Andrew Prince <drewnauts@gmail.com>
2019-07-13 14:44:29 -0400

    adding config file

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {  
+    consumer_key:         'gpcyg1ZW0ZdWEXgPAfvmCMVNw',
+    consumer_secret:      '5M8g23Vd2ycmDVqGj7aBB2knEMx1tgxl7DGAazT1fjsvHsBK0W',
+    access_token:         '1149833142288691202-IAGpUS4hmkKmyqFoj1VBOgEzV9UCat',
+    access_token_secret:  'GXcxuQY7rpDuzmtWyLLOAMnB1EjFs1oibqtKU5hcIumEa',
+}
